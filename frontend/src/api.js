4b622145db35f83248b4d13e718bd906f9fcb442
Jinesh Gandhi <jineshgandhi79@gmail.com>
2026-06-18 20:20:13 +0530

    Update API base URL to include '/api' path

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ai-customer-support-56nr.onrender.com/'
+  baseURL: 'https://ai-customer-support-56nr.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
