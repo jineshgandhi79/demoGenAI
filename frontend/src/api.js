9de1a22dd843ad3394fde0c06101399af3a2ea8b
Jinesh Gandhi <jineshgandhi79@gmail.com>
2026-06-18 20:03:39 +0530

    api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://ai-customer-support-56nr.onrender.com/'
 });
 
 api.interceptors.request.use((config) => {
